5721b925801b575e574b5eb0e4065a2e47f8299e
B.Billion <buhebillion@gmail.com>
2025-04-03 11:19:12 +0200

    was haben wir getan

--- celcFahr/forFahrToCelc/forFahrToCelc.c
+++ celcFahr/forFahrToCelc/forFahrToCelc.c
@@ -10,6 +10,6 @@ int main(int argc, char const *argv[])
 
 int fahr;
 
-for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+for (fahr = LOWERLIMIT; fahr <= UPPERLIMIT; fahr += STEPSIZE)
   printf("%3d %6.1f\n",fahr, (5.0/9.0)*(fahr-32) );
 }
