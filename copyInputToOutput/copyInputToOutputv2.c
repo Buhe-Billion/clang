9bc3f238f11abb1816149d04a60067f0ffefbaff
B.Billion <buhebillion@gmail.com>
2025-04-05 01:17:52 +0200

    Kommentar

--- copyInputToOutput/copyInputToOutputv2.c
+++ copyInputToOutput/copyInputToOutputv2.c
@@ -9,8 +9,12 @@ int c;
 
 // we had to put the assignment operation under () coz of operator precedence
 //The precedence of != is higher than that of =
+/*
+* so:
+* while (c = getchar() != EOF)
+* sets c to 0 or 1, depending on whether or not the call of getchar encountered end of file.
+*/
 while ((c = getchar()) != EOF)
   putchar(c);
-
 
 }
