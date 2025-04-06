5952b2e16b95578b6e156d1f6244d0e5092a3c69
B.Billion <buhebillion@gmail.com>
2025-04-06 12:22:07 +0200

    logic

--- copyInputToOutput/Aufgabe/oneDashSix.c
+++ copyInputToOutput/Aufgabe/oneDashSix.c
@@ -1,3 +1,16 @@
 //Verify that the expression
 //getchar() != EOF
-//is 0 or I.
+//is 0 or 1.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+  int c;
+
+  while ( c = getchar() !=  EOF )
+    printf("%d\n",c);
+  printf("%d - at EOF\n",c);
+
+}
