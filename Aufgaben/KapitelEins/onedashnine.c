d88cecc57240356500dc55643d55aaaa4e63d5ca
B.Billion <buhebillion@gmail.com>
2025-04-10 00:55:27 +0200

    check

---
+++ Aufgaben/KapitelEins/onedashnine.c
@@ -0,0 +1,22 @@
+//create a copy that contracts multiple spaces into a single space
+
+#define CHARACTERSEED 'Q'         //use this to initialise to a non-blank character
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+long character, previousCharacter;
+
+previousCharacter = CHARACTERSEED;
+while ( (character = getchar()) != EOF )
+{
+  if (character != ' ')
+    putchar(character);
+  else if (character == ' ')
+    if (previousCharacter != ' ')
+      putchar(character);
+  previousCharacter = character;
+}
+
+}
