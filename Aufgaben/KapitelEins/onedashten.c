7e73cbd13d62566f7d9c9acd499726a6c7343561
B.Billion <buhebillion@gmail.com>
2025-04-11 09:27:02 +0200

    1st pass

--- Aufgaben/KapitelEins/onedashten.c
+++ Aufgaben/KapitelEins/onedashten.c
@@ -1,2 +1,20 @@
 //Copy input to Output replacing each tab by \t, each backspace by \b, and each
 //backslash by \\.Thus making tabs and backspaces unambigiously.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+long character;
+
+while ((character = getchar()) != EOF)
+  if (character == '\\')
+    printf("\\\\");
+  else if (character == '\b')
+    printf("\\b");
+  else if (character == '\t')
+    printf("\\t");
+  else putchar(c);
+
+}
