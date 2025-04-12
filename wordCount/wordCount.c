0459a5a3583672dd31dc2f76c445d98741db5775
B.Billion <buhebillion@gmail.com>
2025-04-13 00:35:06 +0200

    setup

--- wordCount/wordCount.c
+++ wordCount/wordCount.c
@@ -1,3 +1,10 @@
 //Let us define a word such that it is any sequence of characters that does not
 //contain a blank, tab or new line.
 //Please note that there exists already a sophisticated UNIX implementation: wc
+
+#include <stdio.h>
+
+#define IN 1        //Sate: Inside a word
+#define OUT 0       //State: Outside a word
+
+//count lines, words and characters in input
