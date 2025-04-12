6c6940ec7b9f136b6dfdcd408697164aec4a49ca
B.Billion <buhebillion@gmail.com>
2025-04-13 00:32:18 +0200

    1. Kommentar

---
+++ wordCount/wordCount.c
@@ -0,0 +1,3 @@
+//Let us define a word such that it is any sequence of characters that does not
+//contain a blank, tab or new line.
+//Please note that there exists already a sophisticated UNIX implementation: wc
