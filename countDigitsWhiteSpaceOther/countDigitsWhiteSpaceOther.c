dead6efd822e763fde7ba2e734a3417f0840eeb9
B.Billion <buhebillion@gmail.com>
2025-04-15 00:37:49 +0200

    phase-one

--- countDigitsWhiteSpaceOther/countDigitsWhiteSpaceOther.c
+++ countDigitsWhiteSpaceOther/countDigitsWhiteSpaceOther.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 //count digits, whitespace and other chars
-
 long main(int argc, char const *argv[])
 {
 
@@ -12,6 +11,16 @@ nwhite = nother = 0;
 for (i = 0; i < 10; ++i)
   ndigit[i] = 0;
 
+while ((c = getchar()) != EOF)
+  if (c >= '0' && c <= '9')
+    ++ndigit[c - '0'];
+  else if (c == ' ' || c == '\n' || c == '\t')
+    ++nwhite;
+  else ++nother;
 
+printf("digits = ");
+for (i = 0; i < 10; ++i)
+  printf(" %ld",ndigit[i]);
+printf(", whitespace = %ld, other = %ld\n",nwhite, nother);
 
 }
