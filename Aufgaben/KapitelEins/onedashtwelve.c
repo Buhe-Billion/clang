ef3f4b001fa113d95fb31be86456989fd52a14c2
B.Billion <buhebillion@gmail.com>
2025-04-14 10:15:34 +0200

    first pass

--- Aufgaben/KapitelEins/onedashtwelve.c
+++ Aufgaben/KapitelEins/onedashtwelve.c
@@ -1,1 +1,35 @@
-//This programm prints one word, from its input, per line. 
+//This programm prints one word, from its input, per line.
+
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main(int argc, char const *argv[])
+{
+
+long character, state;
+
+state = OUT;
+while ((character = getchar()) != EOF)
+{
+
+  if ( c == ' ' || c == '\n' || c == '\t' )
+  {
+    if (state)
+    {
+      putchar('\n');
+      state = OUT;
+    }
+  }
+
+  else if (!state)
+  {
+    state == IN;
+    putchar(c);
+  }
+
+  else putchar(c);
+}
+
+}
