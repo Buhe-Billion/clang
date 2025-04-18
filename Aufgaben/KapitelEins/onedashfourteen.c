2f493348f0225dfd65eb8f68e6851ef5e9de72b3
B.Billion <buhebillion@gmail.com>
2025-04-19 00:49:41 +0200

    Kommentar

--- Aufgaben/KapitelEins/onedashfourteen.c
+++ Aufgaben/KapitelEins/onedashfourteen.c
@@ -13,7 +13,7 @@ int main(int argc, char const *argv[])
 {
   int c, i;
   int len;                        //length of each bar
-  int maxvalue;
+  int maxvalue;                   //maximum value for cc[]
   int cc[MAXCHAR];
 
   for (i=0; i<MAXCHAR; ++i)        //There has to be a macro for initialising arrays
