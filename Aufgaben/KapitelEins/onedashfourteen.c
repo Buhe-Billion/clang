a63ae55225f6413f402bf139eeb63bda4ba2f903
B.Billion <buhebillion@gmail.com>
2025-04-19 00:26:20 +0200

    Foundation

--- Aufgaben/KapitelEins/onedashfourteen.c
+++ Aufgaben/KapitelEins/onedashfourteen.c
@@ -1,2 +1,16 @@
 //Write a program to print a histogram of the frequencies of different
 //characters in its input.
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAXHIST 15
+#define MAXCHAR 120
+
+//Print horizontal histogram freq. of different characters
+
+int main(int argc, char const *argv[])
+{
+  
+  return 0;
+}
