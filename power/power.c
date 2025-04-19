531f7f1ba3818c8caa3e6187d9966500728c042d
B.Billion <buhebillion@gmail.com>
2025-04-20 00:38:01 +0200

    Kommentar

--- power/power.c
+++ power/power.c
@@ -11,6 +11,9 @@ int main(int argc, char const *argv[])
   long i;
 
   for (i=0; i < 300; ++i)
+  //we get negative numbers for power(2,i)!!! Coz long is signed??
+  //power(2,i) ends at 2 ^ 63 from 2 ^ 0; 0s after that
+  //power(-3,i) continues to print values
     printf("%ld %ld %ld \n", i, power(2,i), power(-3,i));
   return 0;
 }
