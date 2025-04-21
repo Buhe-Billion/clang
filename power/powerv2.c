378c2e356f423d1544c3f419e043a41d4ba63a5f
B.Billion <buhebillion@gmail.com>
2025-04-22 01:20:03 +0200

    default types check

--- power/powerv2.c
+++ power/powerv2.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-
+int power (int, int)
 
 int main(int argc, char const *argv[])
 {
@@ -11,4 +11,14 @@ int main(int argc, char const *argv[])
   for (i=0; i < 32; ++i)
     printf("%d %d %d \n", i, power(2,i), power(-3,i));
   return 0;
+}
+
+power (base, n)
+{
+  int p;
+// this works coz  n is called by value, thus unchanged in calling function
+// we only change the copy of arguments we have. 
+  for (p = 1; n > 0; --n)
+    p *= base
+  return p;
 }
