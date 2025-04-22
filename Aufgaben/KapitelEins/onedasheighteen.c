9dff7d12177f9913bf1885543e2355144f403f88
B.Billion <buhebillion@gmail.com>
2025-04-22 08:42:39 +0200

    remove fn

--- Aufgaben/KapitelEins/onedasheighteen.c
+++ Aufgaben/KapitelEins/onedasheighteen.c
@@ -19,3 +19,27 @@ int main(int argc, char const *argv[])
 			printf("%s", line);
 	return 0;
 }
+
+/* Remove trailing blanks and tabs from character string s */
+remove (char s[])
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\n')	/* Seek the new line character */
+		++i;
+	--i;					/* One back from '\n' */
+
+	while ( i >= 0 && (s[i] == ' ' || s[i] == '\t') )
+		--i;
+
+	if ( i >= 0 )			/* Ensure that its a non blank line */
+	{
+		++i;
+		s[i] = '\n';		/*	Reinstate newline character */
+		++i;
+		s[i] = '\0';		/* Terminate the string */
+	}
+
+	return i;
+}
