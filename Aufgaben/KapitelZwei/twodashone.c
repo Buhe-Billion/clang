5ad770a1a7600795e65e51dea56bcadf812f20d5
B.Billion <buhebillion@gmail.com>
2025-04-25 14:50:49 +0200

    looks neat, but I bet that it would look way better in capitals

--- Aufgaben/KapitelZwei/twodashone.c
+++ Aufgaben/KapitelZwei/twodashone.c
@@ -16,19 +16,19 @@ int main(int argc, char const *argv[])
 	printf("signed char min \t = \t %d  \t\t\tor in hex: \t%x\n", SCHAR_MIN, SCHAR_MIN);
 	printf("signed char max \t = \t %d  \t\t\tor in hex: \t%x\n", SCHAR_MAX, SCHAR_MAX);
 
-	printf("signed short min \t = \t %d  \t\tor in hex: \t\t\t%x\n", SHRT_MIN, SHRT_MIN);
-	printf("signed short max \t = \t %d  \t\tor in hex: \t\t\t%x\n", SHRT_MAX, SHRT_MAX);
+	printf("signed short min \t = \t %d  \t\tor in hex: \t%x\n", SHRT_MIN, SHRT_MIN);
+	printf("signed short max \t = \t %d  \t\tor in hex: \t%x\n", SHRT_MAX, SHRT_MAX);
 
-	printf("signed int min \t\t = \t %d  \t\tor in hex: \t\t%x\n", INT_MIN, INT_MIN);
-	printf("signed int max \t\t = \t %d  \t\tor in hex: \t\t%x\n", INT_MAX, INT_MAX);
+	printf("signed int min \t\t = \t %d  \t\tor in hex: \t%x\n", INT_MIN, INT_MIN);
+	printf("signed int max \t\t = \t %d  \t\tor in hex: \t%x\n", INT_MAX, INT_MAX);
 
 	printf("signed long min \t = \t %ld  \tor in hex: \t%lx\n", LONG_MIN, LONG_MIN);
 	printf("signed long max \t = \t %ld  \tor in hex: \t%lx\n", LONG_MAX, LONG_MAX);
 
 	/* unsigned types*/
-	printf("unsigned char max \t = \t %u  \t\t\tor in hex: \t\t\t%x\n", UCHAR_MAX, UCHAR_MAX);
-	printf("unsigned short max \t = \t %u  \t\tor in hex: \t\t\t%x\n", USHRT_MAX, USHRT_MAX);
-	printf("unsigned int max \t = \t %u  \t\tor in hex: \t\t%x\n", UINT_MAX, UINT_MAX);
+	printf("unsigned char max \t = \t %u  \t\t\tor in hex: \t%x\n", UCHAR_MAX, UCHAR_MAX);
+	printf("unsigned short max \t = \t %u  \t\tor in hex: \t%x\n", USHRT_MAX, USHRT_MAX);
+	printf("unsigned int max \t = \t %u  \t\tor in hex: \t%x\n", UINT_MAX, UINT_MAX);
 	printf("unsigned long max \t = \t %lu  \tor in hex: \t%lx\n", ULONG_MAX, ULONG_MAX);
 
 	return 0;
