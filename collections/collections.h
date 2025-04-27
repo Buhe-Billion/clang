364c999c2f9dcc5efee2bdfc764d44bc21b028dd
B.Billion <buhebillion@gmail.com>
2025-04-27 16:19:22 +0200

    Kommentar

--- collections/collections.h
+++ collections/collections.h
@@ -429,6 +429,12 @@ int bitcount (unsigned x)
 *machine the program is run on.
 */
 
+/*
+*twodashnine
+*Aufgabe: In a two's complement number system, x &= (x-1) deletes the rightmost 
+*1-bit in x. Explain why. Use this observation to write a faster version of
+*bit-count.
+*/
 /* _bitcount: count 1 bits in x ; faster version */
 int _bitcount (unsigned x)
 {
