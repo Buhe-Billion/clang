91e92f19dab893a2d0529cfbc92e9eb6f14a1a8f
B.Billion <buhebillion@gmail.com>
2025-04-27 15:33:00 +0200

    wordlength

--- collections/collections.h
+++ collections/collections.h
@@ -379,3 +379,14 @@ unsigned rightrot (unsigned x, int n)
 
 		return x;
 }
+
+/* wordlength: computes word length of the machine */
+int wordlength (void)
+{
+		int i;
+		unsigned v = (unsigned) ~0;
+
+		for (i = 1; (v = v >> 1) > 0; i++)
+			;
+		return i;
+}
