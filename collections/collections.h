ba1f95aedf4726e9784688165d84109b74d09e27
B.Billion <buhebillion@gmail.com>
2025-04-28 01:13:51 +0200

    reverse

--- collections/collections.h
+++ collections/collections.h
@@ -577,9 +577,5 @@ void reverse (char s[])
 		int c, i, j;
 
 		for ( i = 0, j = strlen(s) - 1; i < j; i++, j--)
-		{
-			c = s[i];
-			s[i] = s[j];
-			s[j] = c;
-		}
+			c = s[i], s[i] = s[j], s[j] = c;
 }
