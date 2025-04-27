1e335ecc72ded0a656aab634a328495409934fdf
B.Billion <buhebillion@gmail.com>
2025-04-27 13:41:12 +0200

    any()

--- collections/collections.h
+++ collections/collections.h
@@ -30,6 +30,7 @@ void squeeze (char* , int);
 void _strcat (char* , char* );
 char* ptr__strcat (char* , char* );
 void squeeze_str (char* ,char* );
+int any (char* , char* );
 
 /* defines */
 #define MAXLINE 1000		/* Define maximum input line size */
@@ -296,4 +297,24 @@ void squeeze_str (char s1[], char s2[] )
 		}
 
 		s1[k] = '\0';
+}
+
+/*
+*twodashfive
+*Aufgabe: Write the function any(s1,s2), which returns the first location in the string
+*s1 where any character from the string s2 occurs, or -1 if s1 contains no characters
+*from s2. (The standard library function strpbrk does the same job but returns a pointer
+* to the location.)
+*/
+/* any: return first location in s1 where any char from s2 occurs */
+any (char s1[], char s2[] )
+{
+		int i, j;
+
+		for (i = 0; s1[i] != '\0'; i++)
+			for (j = 0; s2[j] != '\0'; j++)
+				if (s1[i] == s2[j])		/* match found */
+					return i;						/* location first match */
+
+		return -1;								/* otherwise, no match */
 }
