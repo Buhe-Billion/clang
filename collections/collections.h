c96400dae15207351bcfa9f988cc1af7087af821
B.Billion <buhebillion@gmail.com>
2025-04-27 09:31:06 +0200

    limits

--- collections/collections.h
+++ collections/collections.h
@@ -6,16 +6,16 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <limits.h>
 
 typedef unsigned char* byte_pointer;
 
-/*From printLongestLinev2.c*/
+/* Function declarations of all the functions we have here */
 int getline (void);
 void copy (void);
-/* from byte_representation.h */
 void show_bytes ();
-/* from atoi.h */
 int atoi (char* );
+void print_limits (void);
 
 
 #define MAXLINE 1000		/* Define maximum input line size */
@@ -84,4 +84,28 @@ int atoi (char s[])
 	for (i = 0; s[i] >= '0' && 	s[i] <= '9'; ++i)
 		n = 10 * n + (s[i] - '0');
 	return n;
+}
+
+/* from twodashone.c */
+void print_limits (void)
+{
+	/* signed types */
+	printf("signed char min \t = \t %d  \t\t\tor in hex: \t%X\n", SCHAR_MIN, SCHAR_MIN);
+	printf("signed char max \t = \t %d  \t\t\tor in hex: \t%X\n", SCHAR_MAX, SCHAR_MAX);
+
+	printf("signed short min \t = \t %d  \t\tor in hex: \t%X\n", SHRT_MIN, SHRT_MIN);
+	printf("signed short max \t = \t %d  \t\tor in hex: \t%X\n", SHRT_MAX, SHRT_MAX);
+
+	printf("signed int min \t\t = \t %d  \t\tor in hex: \t%X\n", INT_MIN, INT_MIN);
+	printf("signed int max \t\t = \t %d  \t\tor in hex: \t%X\n", INT_MAX, INT_MAX);
+
+	printf("signed long min \t = \t %ld  \tor in hex: \t%lX\n", LONG_MIN, LONG_MIN);
+	printf("signed long max \t = \t %ld  \tor in hex: \t%lX\n", LONG_MAX, LONG_MAX);
+
+	/* unsigned types*/
+	printf("unsigned char max \t = \t %u  \t\t\tor in hex: \t%X\n", UCHAR_MAX, UCHAR_MAX);
+	printf("unsigned short max \t = \t %u  \t\tor in hex: \t%X\n", USHRT_MAX, USHRT_MAX);
+	printf("unsigned int max \t = \t %u  \t\tor in hex: \t%X\n", UINT_MAX, UINT_MAX);
+	printf("unsigned long max \t = \t %lu  \tor in hex: \t%lX\n", ULONG_MAX, ULONG_MAX);
+
 }
