fdefb6e590313954f8f44108e00b82ddba19c212
B.Billion <buhebillion@gmail.com>
2025-04-28 01:41:05 +0200

    domain expansion

--- collections/collections.h
+++ collections/collections.h
@@ -49,6 +49,7 @@ void escape (char* , char* );
 void unescape (char* , char* );
 void shellsort (int* , int);
 void reverse (char* );
+void expand (char* , char* );
 
 /* defines */
 
@@ -578,4 +579,33 @@ void reverse (char s[])
 
 		for ( i = 0, j = strlen(s) - 1; i < j; i++, j--)
 			c = s[i], s[i] = s[j], s[j] = c;
+}
+
+/*
+*threedashthree
+*Aufgabe: Write a function expand(s1,s2) that expands shorthand notations 
+*like a-z in the string s1 into the equivalent complete list abc...xyz in s2.
+*Allow for letters of either case and digits, and be prepared to handle
+*cases like a-b-c and a-z0-9 and -a-z.Arrange that a leading or trailing -
+* is taken literally.
+*/
+
+/* expand: expand shorthand notation in s1 into the string s2 */
+void expand (char s1[], char s2[])
+{
+		char c;
+		int i,j;
+
+		i = j = 0;
+		while ((c = s1[i++]) != '\0') /* fetch a char from s1[] */
+			if (s1[i] == '-' && s1[i+1] >= c)
+			{
+				i++;
+				while (c < s1[i])					/* expand shorthand */
+					s2[j++] = c++;
+			}
+			else 
+				s2[j++] = c;							/* copy the character */
+
+		s2[j] = '\0';
 }
