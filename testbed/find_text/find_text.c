c46caa7b9541dd6ea4d1ecf40042af2153cfc455
B.Billion <buhebillion@gmail.com>
2025-04-28 16:15:19 +0200

    find_text

---
+++ testbed/find_text/find_text.c
@@ -0,0 +1,19 @@
+#include "../../collections/collections.h"
+
+const char* pattern = "ould"; /* pattern to search for */
+
+/* find all lines matching the pattern */
+
+int main(int argc, char const *argv[])
+{
+	char line[MAXLINE];
+	int found = 0;
+
+	while (getline_(line,MAXLINE) > 0)
+		if (strindex(line,pattern) >= 0)
+		{
+			printf("%s", line);
+			found++;
+		}
+	return found;
+}
