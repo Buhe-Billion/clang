08d3804437ec4a27b47cce61dd0f4039e52e5691
B.Billion <buhebillion@gmail.com>
2025-04-28 16:50:47 +0200

    strrindex

--- collections/collections.h
+++ collections/collections.h
@@ -57,6 +57,7 @@ void itob (int, char* ,int);
 void _itoa_ (int, char* ,int);
 int getline_ (char* , int);
 int strindex (char* , char* );
+int strrindex (char* , char*);
 
 
 /* defines */
@@ -774,4 +775,27 @@ int strindex (char s[], char t[])
 		}
 
 		return -1;
+}
+
+/*
+*fourdashone
+*Aufgabe: Write the function strrindex(s,t), which returns the position of the 
+*rightmost occurance of t in s, or -1 if there is none.
+*/
+
+/* strrindex: returns rightmost index of t in s, -1 if none */
+int strrindex (char s[],char t[])
+{
+		int i, j, k, pos;
+
+		pos = -1;
+		for (i=0; s[i] != '\0'; i++)
+		{
+				for (j=1,k=0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+					;
+				if (k > 0 && t[k] == '\0')
+					pos = i;
+		}
+
+		return pos;
 }
