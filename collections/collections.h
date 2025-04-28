a4823ca49edd6f375ec1ad6b0b0e00db9be13ea5
B.Billion <buhebillion@gmail.com>
2025-04-28 16:55:56 +0200

    efficiency gains

--- collections/collections.h
+++ collections/collections.h
@@ -784,6 +784,7 @@ int strindex (char s[], char t[])
 */
 
 /* strrindex: returns rightmost index of t in s, -1 if none */
+/*
 int strrindex (char s[],char t[])
 {
 		int i, j, k, pos;
@@ -798,4 +799,22 @@ int strrindex (char s[],char t[])
 		}
 
 		return pos;
+}
+*/
+
+/* A more efficient approach : */
+int strrindex (char s[],char t[])
+{
+		int i, j, k;
+
+		for (i = strlen(s) - strlen(t); i >= 0; i--)
+		{
+				for (j =1,k=0; t[k] != '\0' && s[j] == t[k]; j++,k++)
+					;
+
+				if (k > 0 && t[k] == '\0')
+					return i;
+		}
+
+		return -1;
 }
