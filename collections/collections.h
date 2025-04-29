f432f46c7fb5fa9860dbf67f7f5ab9dc70e416d6
B.Billion <buhebillion@gmail.com>
2025-04-29 02:39:21 +0200

    _atof_

--- collections/collections.h
+++ collections/collections.h
@@ -829,7 +829,7 @@ int strrindex (char s[],char t[])
 double _atof_ (char s[])
 {
 		double val, power;
-		int i,sign;
+		int exp, i, sign;
 
 		for (i=0; isspace(s[i]); i++)	/* skip whitespace */
 			;
@@ -847,5 +847,23 @@ double _atof_ (char s[])
 			power *= 10.0;
 		}
 
-		return sign * val / power; /*Double bang! What a play*/
+		val = sign * val / power; /*Double bang! What a play*/
+
+		if (s[i] == 'e' || s[i] == 'E')
+		{
+			sign = (s[i++] == '-') ? -1 : 1;
+			if (s[i] == '+' || s[i] == '-')
+				i++;
+			for (exp = 0; isdigit(s[i]); i++)
+				exp = 10 * exp + (s[i] - '0');
+			if (sign == 1)
+				while (exp-- > 0)		/* positive exponent */
+					val *= 10;
+			else
+				while (exp-- > 0)		/* negative exponent */
+					val /= 10;
+		}
+
+		return val
 }
+
