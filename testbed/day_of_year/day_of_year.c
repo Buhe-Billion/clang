6c64da2d61c2c8683c8acab7995e1bd1a58318f0
B.Billion <buhebillion@gmail.com>
2025-05-03 13:30:17 +0200

    track

--- testbed/day_of_year/day_of_year.c
+++ testbed/day_of_year/day_of_year.c
@@ -10,21 +10,23 @@ int main(int argc, char const *argv[])
 	int* pmonth, pday;
 	*
 	*/
+	integer_pointer pmonth, pday;
 
 	/* Pointers have to have something to point to and -1 is a nonsense value that indicates an error*/
-	signed day = -1;
-	signed month = -1;
-
-	integer_pointer pmonth, pday;
+	signed month, day = -1;
 
 	pmonth = &day;
 	pday = &month;
-
 	
 	printf("This is the: %d th day of the year\n", day_of_year(2025, 5, 3));
 
+	/*
 	month_day(2025, 123, pmonth, pday);
 	printf("The 123rd day of 2025, vandaag, is on the %drd day of the %dth month \n", *pday, *pmonth);
-	
+	*/
+
+	month_day(2025, 123, &month, &day);
+	printf("The 123rd day of 2025, vandaag, is on the %drd day of the %dth month \n", day, month);
+
 	return 0;
 }
