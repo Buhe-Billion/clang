2ca90b6ddd9802a89fba59bd19e20927210a1c88
B.Billion <buhebillion@gmail.com>
2025-05-03 13:22:06 +0200

    track

--- testbed/day_of_year/day_of_year.c
+++ testbed/day_of_year/day_of_year.c
@@ -2,6 +2,29 @@
 
 int main(int argc, char const *argv[])
 {
+	
+	/*
+	*
+	* Line below makes only pmonth to be a pointer!
+	*
+	int* pmonth, pday;
+	*
+	*/
+
+	/* Pointers have to have something to point to and -1 is a nonsense value that indicates an error*/
+	signed day = -1;
+	signed month = -1;
+
+	integer_pointer pmonth, pday;
+
+	pmonth = &day;
+	pday = &month;
+
+	
 	printf("This is the: %d th day of the year\n", day_of_year(2025, 5, 3));
+
+	month_day(2025, 123, pmonth, pday);
+	printf("The 123rd day of 2025, vandaag, is on: %d th month and on the %d rd day\n", *pmonth, *pday);
+	
 	return 0;
 }
