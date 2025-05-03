19af66f559766abb2ba1a282020f2b3a87632ed2
B.Billion <buhebillion@gmail.com>
2025-05-03 02:44:43 +0200

    track changes

--- collections/collections.h
+++ collections/collections.h
@@ -137,6 +137,12 @@ static int bufp = 0;			/* next free position in buf */
 static char allocbuf[ALLOCSIZE]; /* storage for alloc */
 static char* allocp = allocbuf;	 /* next free position*/
 
+static char daytab[2][13] =
+{
+	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
 
 
 /* Foundational set ups; end */
